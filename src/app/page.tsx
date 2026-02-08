98526a6f5bb04a5885e76feb3de606d2f4a3f61a
oyonbdcom <oyonbd.com@gmail.com>
2026-02-08 18:55:59 +0600

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import LandingPageView from "@/components/landing-page";
 import LayoutLoader from "@/components/layout-loader";
 
 import { Suspense } from "react";
-
+const page = "";
 export default function Page() {
   return (
     // 1. Wrap the component that uses useSearchParams in Suspense
